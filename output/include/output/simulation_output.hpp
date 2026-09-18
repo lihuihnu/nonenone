35e642a2ac74de0b84ac3079e281c4eb3ec0087c
lihuihnu <346848032lihui@gmail.com>
2026-09-18 10:37:20 +0800

    feat(output): export producer composition simulation output

--- output/include/output/simulation_output.hpp
+++ output/include/output/simulation_output.hpp
@@ -8,5 +8,6 @@
 #include <output/simulation/model_inventory_output.hpp>
 #include <output/simulation/component_mass_balance_output.hpp>
 #include <output/well/detailed_well_output.hpp>
+#include <output/well/producer_composition_output.hpp>
 #include <output/simulation/reservoir_diagnostics.hpp>
 #include <output/well/control_switch_output.hpp>
